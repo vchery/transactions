e50f7c789361fce4005008558821f4225e90ec4b
valerie chery <87587798+vchery@users.noreply.github.com>
2024-11-28 17:27:07 -0500

    changed variable names, added more comments detailing functionality

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 class Transaction:
-    db = dict()
-    transactions = dict()
-    ongoing = False
+    db = dict()            # data will be committed to this database
+    transactions = dict()  # pending transactions are stored here before a commit
+    ongoing = False        # flag for detecting an ongoing transaction
 
     def __init__(self):
-        print("A new object has been initialized.")
+        return
 
     def begin_transaction(self):
         # starts a new transaction, only 1 can occur at a time
@@ -13,28 +13,33 @@ class Transaction:
             self.ongoing = True
             print("Starting a new transaction.")
         else:
-            raise Exception("A transaction is already in progress.")
+            print("Error: A transaction is already in progress.")
+            # raise Exception("A transaction is already in progress.")
 
     def put(self, key:str, value:int):
         # throw an exception if a transaction hasn't been started
         if self.ongoing == False:
-            raise Exception("A transaction has not been started. Cannot add new data.")
+            print("Error: A transaction has not been started. Cannot add new data.")
+            # raise Exception("A transaction has not been started. Cannot add new data.")
         
         # creates a new key with the provided value if it doesn't already exist
         # otherwise, update the value of an existing key
-        self.transactions[key] = value
+        else:
+            self.transactions[key] = value
 
     def get(self, key:str):
         # return the value associated with the key
         if key in self.db:
             return print(self.db[key])
+        
         # if the key doesn't exist, return null
         else:
             return print(None)
 
     def commit(self):
         if self.ongoing == False:
-            raise Exception("A transaction has not been started. No data to commit.")
+            print("Error: A transaction has not been started. No data to commit.")
+            # raise Exception("A transaction has not been started. No data to commit.")
         
         # add values to the final dictionary
         for k,v in self.transactions.items():
@@ -56,18 +61,19 @@ class Transaction:
         print("Removed uncommitted changes.")
 
 if __name__ == '__main__':
-    t1 = Transaction()
-    t1.get("A")
-    # print(t1.put("A",5))
-    t1.begin_transaction()
-    t1.put("A",5)
-    print(t1.get("A"))
-    t1.put("A",6)
-    t1.commit()
-    t1.get("A")
-    # t1.commit()
-    t1.get("B")
-    t1.begin_transaction()
-    t1.put("B",10)
-    t1.rollback()
-    t1.get("B")
+    t = Transaction()
+    t.get("A")
+    t.put("A",5)
+    t.begin_transaction()
+    t.put("A",5)
+    t.get("A")
+    t.put("A",6)
+    t.commit()
+    t.get("A")
+    t.commit()
+    t.rollback()
+    t.get("B")
+    t.begin_transaction()
+    t.put("B",10)
+    t.rollback()
+    t.get("B")
